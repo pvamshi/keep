93bb705ab2e4191ad9c0e5bc11f7319a3618c397
Vamshi Krishna <pvamshi@gmail.com>
2014-08-09 09:14:08 +0530

    STEP 3 : Using controller in the directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,8 +1,13 @@
-'use strict'
+'use strict';
 angular
     .module('keepApp')
     .directive('test1', function() {
         return {
-            templateUrl: 'views/test1.html'
+            templateUrl: 'views/test1.html',
+            controller: function($scope) {
+                $scope.saveName = function() {
+                    console.log("Saving name of value:" + $scope.name);
+                };
+            }
         };
     });
